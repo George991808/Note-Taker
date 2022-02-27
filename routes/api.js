d716a7201b3fd27f4f043a4fb4551fb30267e475
George991808 <gellis7@gmail.com>
2022-02-27 15:29:17 +0800

    api.js gets route to db.josn

---
+++ routes/api.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const path = require('path');
+const fs =require('fs');
+
+
+
+router.get('/api/notes',(req,res)=>{
+   
+const dbPath = path.join(__dirname, "..", 'db', 'db.json');
+
+const content = JSON.parse( fs.readFileSync(dbPath,'utf-8') || "[]");
+console.log(content)
+}
+    
+)
+module.exports=router;
--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 
 const app = express();
 const webRoutes = require('./routes/web');
-const appRoutes = require('./routes/web');
+const appRoutes = require('./routes/api');
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 app.use(webRoutes);
+app.use(appRoutes);
 
 app.listen(PORT,() => {
     console.log('app is running on ${PORT} ')
