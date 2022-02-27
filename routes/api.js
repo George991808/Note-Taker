a2ce4a8b445f1eeea26b2c68dbbbfb84a7ed00c4
George991808 <gellis7@gmail.com>
2022-02-27 16:35:20 +0800

    added delete route to delete notes

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,18 @@
 const router = require("express").Router();
 const path = require("path");
 const fs = require("fs");
-const uuid = require('uuid');
+const uuid = require("uuid");
 const { redirect } = require("express/lib/response");
 const dbPath = path.join(__dirname, "..", "db", "db.json");
 
 function getNotes() {
+  return JSON.parse(fs.readFileSync(dbPath, "utf-8") || "[]");
+}
 
-    return JSON.parse(fs.readFileSync(dbPath, "utf-8") || "[]");
+function saveNotes(note){
+    fs.writeFileSync(dbPath, JSON.stringify(note), "utf-8");
 }
 router.get("/api/notes", (req, res) => {
-
-
   const notes = getNotes();
   console.log(notes);
 
@@ -19,24 +20,31 @@ router.get("/api/notes", (req, res) => {
 });
 
 router.post("/api/notes", (req, res) => {
-    const {title, text} =req.body;
-     
-    console.log(title, text);
+  const { title, text } = req.body;
 
-    const newNote = {
-        id: uuid.v4(),
-        title: title,
-        text: text,
-    }
- const existingNotes = getNotes()
+  console.log(title, text);
 
- existingNotes.push(newNote);
+  const newNote = {
+    id: uuid.v4(),
+    title: title,
+    text: text,
+  };
+  const existingNotes = getNotes();
 
- fs.writeFileSync(dbPath,JSON.stringify(existingNotes),"utf-8");
+  existingNotes.push(newNote);
+  saveNotes(existingNotes)
+ 
 
- redirect.json(newNote);
+  res.json(newNote);
+});
 
+router.delete('/api/notes/:id', (req, res)=>{
+const notes = getNotes();
 
-  });
+const filtered = notes.filter((notes) => notes.id !==req.params.id);
+saveNotes(filtered)
+res.json(filtered);
+
+})
 
 module.exports = router;
