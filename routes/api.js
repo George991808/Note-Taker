ce1901eb4a8ad41e6b72c847b8681bdc1e1e7359
George991808 <gellis7@gmail.com>
2022-02-27 15:57:17 +0800

    post route aadded to api.js

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,20 @@
-const router = require('express').Router();
-const path = require('path');
-const fs =require('fs');
+const router = require("express").Router();
+const path = require("path");
+const fs = require("fs");
 
+router.get("/api/notes", (req, res) => {
+  const dbPath = path.join(__dirname, "..", "db", "db.json");
 
+  const content = JSON.parse(fs.readFileSync(dbPath, "utf-8") || "[]");
+  console.log(content);
 
-router.get('/api/notes',(req,res)=>{
-   
-const dbPath = path.join(__dirname, "..", 'db', 'db.json');
+  res.json(content);
+});
 
-const content = JSON.parse( fs.readFileSync(dbPath,'utf-8') || "[]");
-console.log(content);
+router.post("/api/notes", (req, res) => {
+    const {title, text} =req.body;
+     
+    console.log(title, text);
+  });
 
-res.json(content);
-}
-    
-)
-module.exports=router;
+module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const appRoutes = require('./routes/api');
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(webRoutes);
 app.use(appRoutes);
 
